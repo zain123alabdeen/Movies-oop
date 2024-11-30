81ab3dd7912534a39c89c0fa1af75071c458e732
zain123alabdeen <zainalebdeenbader@gmail.com>
2024-11-30 21:05:41 +0300

    Add files via upload
    This is project of choosing movies by python opp

---
+++ movies_list.py
@@ -0,0 +1,39 @@
+# class movies:
+#     def __init__(self,title,director,release_year,gener):
+#         self.title=title
+#         self.director=director
+#         self.release_year=release_year
+#         self.gener=gener
+        
+#     def display(self):
+#         print(f'Title: {self.title}')
+#         print(f'Director: {self.director}')
+#         print(f'Release Year: {self.release_year}')
+#         print(f'Gener: {self.gener}')
+#         print('\n\n')
+#     def updating_data(self,new_directors):
+#         print('Changing Movies Directors........\n\n')
+#         self.director=new_directors
+         
+    
+    
+# first_movie=movies('The Black List','Jon',2011,'Crim')
+# first_movie.display()
+
+    
+# second_movie=movies('Fast and firais','Gogo',2010,'Racing')
+# second_movie.display()
+    
+# third_movie=movies('Love In Island','Mono',2009,'Romantic')
+# third_movie.display()
+
+# first_movie.updating_data('lion')
+# first_movie.display()
+
+# second_movie.updating_data('Ronaldo')
+# second_movie.display()
+
+# third_movie.updating_data('Nansi')
+# third_movie.display() 
+
+   
